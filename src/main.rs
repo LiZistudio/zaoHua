3006c53788c02e371d49172d2997b849d9b06fc5
LiZistudio <1185345172@qq.com>
2023-05-09 15:04:21 +0800

    add zaoHua.txt

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-fn main() {
-    let engine_title:String = String::from("☯☯☯Welcome to zaoHua game engine.☯☯☯");
-    println!("{:?}",engine_title);
-}
+use std::fs;
 
+fn main() {
+    let engine_title:String = String::from("造化钟神秀，阴阳割昏晓。Welcome to zaoHua game engine.");
+    println!("{:?}",engine_title);
+    //println!("In file {}", "zaoHua.txt");
+    let contents = fs::read_to_string("zaoHua.txt")
+        .expect("Something went wrong reading the file");
+    println!("{}", contents);
+}
